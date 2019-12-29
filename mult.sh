84afaa00e2ca86195663d4f77ad4b35f9037e098
The-Receiver <47209193+The-Receiver@users.noreply.github.com>
2019-12-29 15:29:38 +0630

    Added no-truncate option

--- mult.sh
+++ mult.sh
@@ -1,29 +1,35 @@
-#!/bin/bash
+	#!/bin/bash
 
 start=1
-while getopts "ml:" opt; do 
+
+#m: Repeat numbers
+#l: Set lower bound
+#n: Do not truncate to one line
+while getopts "nml:" opt; do 
  case "${opt}" in
-  m) m=1
-	;; 
+  m) m=1;; 
   l) start=$OPTARG;; 
-  *) m=0
-	;;
+  n) trunc=0;;
+  *) m=0; trunc=0;;
   esac
 done
    shift $((OPTIND-1))
 
 repeat=""
-
-
 if [ $m ]
 then
    repeat=-r
 fi
 
-if [ -z $2 ]; then
+if [ $2 -eq 0 ]; then
 shufcmd="shuf -i $start-$1 $repeat -n 1"
 else
 shufcmd="shuf -i $start-$1 $repeat -n $2"
 fi
 
+if [ $trunc -eq 1 ]; then
 $shufcmd | tr '\n' ' ' | head -c -1; printf '\n'
+else
+$shufcmd
+fi	
+
